22de521d8340bf2454a3c5915c6757a30eb52f28
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2022-04-26 13:39:16 +0700

    Store and beginning API code

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,25 @@
+import {
+  createApi,
+  fecthBaseQuery,
+  fetchBaseQuery,
+} from "@reduxjs/toolkit/query";
+
+const cryptoApiHeaders = {
+  "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+  "X-RapidAPI-Key": "064811ba78msh80570e72e5c3709p15de98jsn55229726c265",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com/coins";
+// const baseUrl = "https://coinranking1.p.rapidapi.com/"; Try this one if doesnt work (dif on yt video)
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest("/exchanges"),
+    }),
+  }),
+});
