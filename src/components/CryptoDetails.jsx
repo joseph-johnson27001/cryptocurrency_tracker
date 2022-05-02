266eaa1c687931f1b007ec5828afe168148454cf
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2022-05-02 13:14:31 +0700

    Crypto news & links (styling needs work)

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -28,8 +28,6 @@ const CryptoDetails = () => {
 
   if (isFetching) return "Loading...";
 
-  console.log("cryptodetails", cryptoDetails);
-
   const time = ["3h", "24h", "7d", "30d", "1y", "3m", "3y", "5y"];
 
   const stats = [
@@ -39,7 +37,6 @@ const CryptoDetails = () => {
       icon: <DollarCircleOutlined />,
     },
     { title: "Rank", value: cryptoDetails.rank, icon: <NumberOutlined /> },
-    // BELOW: CRYPTO 24 H Volume failine to load (need to use 24hVolume but creates super fun error)
     {
       title: "24h Volume",
       value: `$ ${
@@ -58,8 +55,6 @@ const CryptoDetails = () => {
       icon: <TrophyOutlined />,
     },
   ];
-
-  console.log("stats", stats);
 
   const genericStats = [
     {
@@ -121,9 +116,9 @@ const CryptoDetails = () => {
         <Col className="coin-value-statistics">
           <Col className="coin-value-statistics-heading">
             <Title level={3} className="coin-details-heading">
-              {cryptoDetails.name} Value Statistics
+              {cryptoDetails.name} Value Stats
             </Title>
-            <p>An overview showing the stats of {cryptoDetails.name}</p>
+            <p>An Overview of {cryptoDetails.name}'s Valuation Statistics</p>
           </Col>
           {stats.map(({ icon, title, value }) => (
             <Col className="coin-stats">
@@ -134,25 +129,49 @@ const CryptoDetails = () => {
               <Text className="stats">{value}</Text>
             </Col>
           ))}
+          {/* </Col> */}
+        </Col>
+        {/* GENERIC STATS AREA */}
+        <Col className="other-stats-info">
+          <Col className="coin-value-statistics-heading">
+            <Title level={3} className="coin-details-heading">
+              Generic {cryptoDetails.name} Information
+            </Title>
+            <p>Generic {cryptoDetails.name} Statistics</p>
+          </Col>
+          {genericStats.map(({ icon, title, value }) => (
+            <Col className="coin-stats">
+              <Col className="coin-stats-name">
+                <Text>{icon}</Text>
+                <Text>{title}</Text>
+              </Col>
+              <Text className="stats">{value}</Text>
+            </Col>
+          ))}
         </Col>
       </Col>
-      {/* GENERIC STATS AREA */}
-      <Col className="other-stats-info">
-        <Col className="coin-value-statistics-heading">
-          <Title level={3} className="coin-details-heading">
-            {cryptoDetails.name} Generic Statistics
+      <Col className="coin-desc-link">
+        <Row className="coin-desc">
+          <Title level={3} className="link-name">
+            What is {cryptoDetails.name}?
+            {HTMLReactParser(cryptoDetails.description)}
           </Title>
-          <p>An overview showing the stats of {cryptoDetails.name}</p>
+        </Row>
+        <Col className="coin-links">
+          <Title level={3} className="coin-details-heading">
+            {cryptoDetails.name} Links
+          </Title>
+          {cryptoDetails.links.map((link) => (
+            <Row className="coin-link" key={link.name}>
+              <Title level={5} className="link-name">
+                {link.type}
+              </Title>
+              <a href={link.url} target="_blank" rel="noreferrer">
+                {link.name}
+              </a>
+            </Row>
+          ))}
         </Col>
-        {genericStats.map(({ icon, title, value }) => (
-          <Col className="coin-stats">
-            <Col className="coin-stats-name">
-              <Text>{icon}</Text>
-              <Text>{title}</Text>
-            </Col>
-            <Text className="stats">{value}</Text>
-          </Col>
-        ))}
       </Col>
     </Col>
   );
