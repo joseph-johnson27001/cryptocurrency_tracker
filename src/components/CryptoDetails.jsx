578b1f048c5014386e008d8d76641065b6a6aff4
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2022-05-02 12:01:16 +0700

    Removed 24 h volume (for now)

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -28,7 +28,7 @@ const CryptoDetails = () => {
 
   if (isFetching) return "Loading...";
 
-  console.log(cryptoDetails);
+  console.log("cryptodetails", cryptoDetails);
 
   const time = ["3h", "24h", "7d", "30d", "1y", "3m", "3y", "5y"];
 
@@ -39,11 +39,12 @@ const CryptoDetails = () => {
       icon: <DollarCircleOutlined />,
     },
     { title: "Rank", value: cryptoDetails.rank, icon: <NumberOutlined /> },
-    {
-      title: "24h Volume",
-      value: `$ ${cryptoDetails.volume && millify(cryptoDetails.volume)}`,
-      icon: <ThunderboltOutlined />,
-    },
+    // BELOW: CRYPTO 24 H Volume failine to load (need to use 24hVolume but creates super fun error)
+    // {
+    //   title: "24h Volume",
+    //   value: `$ ${cryptoDetails.volume && millify(cryptoDetails.volume)}`,
+    //   icon: <ThunderboltOutlined />,
+    // },
     {
       title: "Market Cap",
       value: `$ ${cryptoDetails.marketCap && millify(cryptoDetails.marketCap)}`,
