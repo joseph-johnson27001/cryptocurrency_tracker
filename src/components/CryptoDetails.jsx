710d2a083da3b8a5da2552bf87a7ad53664ac41f
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2022-05-02 13:34:57 +0700

    Crypto line chart (api link wrong)

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -15,7 +15,11 @@ import {
   ThunderboltOutlined,
 } from "@ant-design/icons";
 
-import { useGetCryptoDetailsQuery } from "../services/cryptoApi";
+import {
+  useGetCryptoDetailsQuery,
+  useGetCryptoHistoryQuery,
+} from "../services/cryptoApi";
+import LineChart from "./LineChart";
 
 const { Title, Text } = Typography;
 const { Option } = Select;
@@ -24,6 +28,7 @@ const CryptoDetails = () => {
   const { uuid } = useParams();
   const [timePeriod, setTimePeriod] = useState("7d");
   const { data, isFetching } = useGetCryptoDetailsQuery(uuid);
+  const { data: coinHistory } = useGetCryptoHistoryQuery({ uuid, timePeriod });
   const cryptoDetails = data?.data?.coin;
 
   if (isFetching) return "Loading...";
@@ -111,7 +116,11 @@ const CryptoDetails = () => {
           <Option key={date}>{date}</Option>
         ))}
       </Select>
-      {/* { LINE CHART AREA } */}
+      <LineChart
+        coinHistory={coinHistory}
+        currentPrice={millify(cryptoDetails.price)}
+        coinName={cryptoDetails.name}
+      />
       <Col className="stats-container">
         <Col className="coin-value-statistics">
           <Col className="coin-value-statistics-heading">
