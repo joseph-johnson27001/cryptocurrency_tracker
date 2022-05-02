cac27b7809e0d3cf335e1a65e695eb1ec94537f9
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2022-05-02 15:57:05 +0700

    Chart now loading (no data)

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,6 +1,26 @@
 import React from "react";
 import { Line } from "react-chartjs-2";
 import { Col, Row, Typography } from "antd";
+import {
+  Chart as ChartJS,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Filler,
+  TimeScale,
+} from "chart.js";
+
+import "chartjs-adapter-moment";
+
+ChartJS.register(
+  LinearScale,
+  TimeScale,
+  PointElement,
+  Filler,
+  LineElement,
+  Tooltip
+);
 
 const { Title } = Typography;
 
@@ -8,25 +28,21 @@ const LineChart = ({ coinHistory, currentPrice, coinName }) => {
   const coinPrice = [];
   const coinTimestamp = [];
 
-  for (let i = 0; i < coinHistory?.data?.history.length; i++) {
-    console.log(coinHistory);
-    coinPrice.push(coinHistory.data.history[i].price);
+  for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
+    coinPrice.push(coinHistory?.data?.history[i].price);
   }
 
   for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
     coinTimestamp.push(
-      new Date(coinHistory?.data?.history[i].timestamp).toLocaleString("en-us")
+      new Date(coinHistory?.data?.history[i].timestamp).toLocaleDateString()
     );
   }
 
-  console.log(coinPrice);
-  console.log(coinTimestamp);
-
   const data = {
-    label: coinTimestamp,
+    labels: coinTimestamp,
     datasets: [
       {
-        label: "Price in USD",
+        label: "Price In USD",
         data: coinPrice,
         fill: false,
         backgroundColor: "#0071bd",
@@ -37,24 +53,20 @@ const LineChart = ({ coinHistory, currentPrice, coinName }) => {
 
   const options = {
     scales: {
-      yAxes: [
-        {
-          ticks: {
-            beginAtZero: true,
+      yAxes: {
+        ticks: {
+          beginAtZero: true,
+        },
+      },
+      xAxes: {
+        type: "time",
+        time: {
+          unit: "day",
+          displayFormats: {
+            day: "DD MM YYYY",
           },
         },
-      ],
-      xAxes: [
-        {
-          type: "time",
-          time: {
-            unit: "day",
-            displayFormats: {
-              day: "MMM DD YYYY",
-            },
-          },
-        },
-      ],
+      },
     },
   };
 
@@ -62,11 +74,11 @@ const LineChart = ({ coinHistory, currentPrice, coinName }) => {
     <>
       <Row className="chart-header">
         <Title level={2} className="chart-title">
-          {coinName} Price Chart
+          {coinName} Price Chart{" "}
         </Title>
         <Col className="price-container">
           <Title level={5} className="price-change">
-            {coinHistory?.data?.change}%
+            Change: {coinHistory?.data?.change}%
           </Title>
           <Title level={5} className="current-price">
             Current {coinName} Price: $ {currentPrice}
