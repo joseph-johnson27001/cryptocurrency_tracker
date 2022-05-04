f33fb0f3b069157b7c47965e7b89508104576e7c
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2022-05-04 13:11:53 +0700

    Removed footer

--- src/App.js
+++ src/App.js
@@ -36,19 +36,6 @@ const App = () => {
             </Switch>
           </div>
         </Layout>
-        <div className="footer">
-          <Typography.Title
-            level={5}
-            style={{ color: "white", textAlign: "center" }}
-          >
-            Cryptocurrency Tracker <br />
-            All rights reserved
-          </Typography.Title>
-          <Space>
-            <Link to="/">Home</Link>
-            <Link to="/news">News</Link>
-          </Space>
-        </div>
       </div>
     </div>
   );
