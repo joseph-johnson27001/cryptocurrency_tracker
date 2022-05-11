3d565c5626eeaaa8ebffb08ddc5e0ccec45e35e3
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2022-05-11 13:45:50 +0700

    Fixed nav button movement on screen res change

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -23,6 +23,8 @@ const Navbar = () => {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
+  console.log("state", activeMenu);
+
   return (
     <div className="nav-container">
       <div className="logo-container">
