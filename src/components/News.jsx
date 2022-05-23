348148c301e6a951588f134759386ddca4e345a9
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2022-05-23 13:42:27 +0700

    News Cards correct positioning and styling

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -48,40 +48,42 @@ const News = ({ simplified }) => {
       {cryptoNews.value.map((news, i) => (
         <Col xs={24} lg={8} key={i}>
           <Card hoverable className="news-card">
-            <a href={news.url} target="_blank" rel="noreferrer">
-              <div className="news-image-container">
-                <Title className="news-title" level={4}>
-                  {news.name}
-                </Title>
-                <img
-                  src={news?.image?.thumbnail?.contentUrl || demoImage}
-                  alt="news"
-                  className="news-image"
-                />
-              </div>
-              <p>
-                {news.description > 100
-                  ? `${news.description.substring(0, 100)}...`
-                  : news.description}
-              </p>
-              <div className="provider-container">
-                <div>
-                  <Avatar
-                    src={
-                      news.provider[0]?.image?.thumbnail?.contentUrl ||
-                      demoImage
-                    }
+            <div className="news-card-body">
+              <a href={news.url} target="_blank" rel="noreferrer">
+                <div className="news-image-container">
+                  <Title className="news-title" level={4}>
+                    {news.name}
+                  </Title>
+                  <img
+                    src={news?.image?.thumbnail?.contentUrl || demoImage}
                     alt="news"
+                    className="news-image"
                   />
-                  <Text className="provider-name">
-                    {news.provider[0]?.name}
+                </div>
+                <p>
+                  {news.description > 100
+                    ? `${news.description.substring(0, 100)}...`
+                    : news.description}
+                </p>
+                <div className="provider-container">
+                  <div>
+                    <Avatar
+                      src={
+                        news.provider[0]?.image?.thumbnail?.contentUrl ||
+                        demoImage
+                      }
+                      alt="news"
+                    />
+                    <Text className="provider-name">
+                      {news.provider[0]?.name}
+                    </Text>
+                  </div>
+                  <Text>
+                    {moment(news.datePublished).startOf("ss").fromNow()}
                   </Text>
                 </div>
-                <Text>
-                  {moment(news.datePublished).startOf("ss").fromNow()}
-                </Text>
-              </div>
-            </a>
+              </a>
+            </div>
           </Card>
         </Col>
       ))}
